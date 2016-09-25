b5cf0ab2a341fa8b925c8fd019fd6fcc0544ddb6
sugatmahanti <sugatmahanti@gmail.com>
2016-09-25 12:05:26 -0400

    First Commit
    Committing the working solution for PostGreSQL Core library.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using ServiceStack.OrmLite;
+using ServiceStack.Text;
+
+namespace ConsoleApplication
+{
+    public class sample{
+       
+        public int id{get;set;}
+        public string Name{get;set;}
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           string connectionString ="UserID=smahanti;Host=localhost;Port=5432;Database=test;";
+          var dbFactory = new OrmLiteConnectionFactory(
+    connectionString,  
+    PostgreSqlDialect.Provider);
+    using (var db = dbFactory.Open())
+{
+    if (db.CreateTableIfNotExists<sample>())
+    {
+        db.Insert(new sample { id = 1, Name = "Seed Data"});
+    }
+    var result = db.Single<sample>(1);
+    result.PrintDump(); 
+}
+        }
+    }
+}
